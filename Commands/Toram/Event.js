51cff42dfa0309b0255cc00d3ab6a7985021a01e
Sriend <happy.sriend@gmail.com>
2020-07-17 13:07:52 +0700

    Save Latest Work

---
+++ Commands/Toram/Event.js
@@ -0,0 +1,14 @@
+const Discord    = require("discord.js");
+module.exports = {
+    name: "potum news",
+    aliases: ["p news","p n"],
+    group: "Toram",
+    desc: "Show Current Active news",
+    usage: "<input>",
+    run: async (client, message, args)=>{
+        let embed = new Discord.MessageEmbed();
+        embed.setTitle("5th Anniversary Event");
+        embed.setDescription(``);
+        message.channel.send(embed);
+    }
+}
