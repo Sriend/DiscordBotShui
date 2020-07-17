f5680086cf4d1f72567aeba40f1445fef3de0c90
Sriend <happy.sriend@gmail.com>
2020-07-17 11:49:39 +0700

    CMD Avatar

---
+++ Commands/Info/Avatar.js
@@ -0,0 +1,14 @@
+const Discord    = require("discord.js");
+module.exports = {
+    name: "avatar",
+    aliases: "",
+    group: "Info",
+    desc: "Show your own Avatar",
+    usage: "avatar <tagname>",
+    run: async (client, message, args)=>{
+        let embed = new Discord.MessageEmbed();
+        embed.setTitle(message.author.username);
+        embed.setDescription(message.author.displayAvatarURL());
+        message.channel.send(embed);
+    }
+}
