8eb098438bd8be4926b168d9e6bd28e71bf52cf7
Sriend <happy.sriend@gmail.com>
2020-07-18 12:43:52 +0700

    add console

--- Commands/Toram/Farm.js
+++ Commands/Toram/Farm.js
@@ -8,6 +8,7 @@ module.exports = {
     run: async (client, message, args)=>{
         let farm = new Array("metal","cloth","beast","wood","medicine","mana");
         let name = farm.filter(f=>{ return f === args[0];});
+        console.log(name);
         if(!name) return message.channel.send("input farm list!");
         let embed = new Discord.MessageEmbed();
         //let data  = require("");
