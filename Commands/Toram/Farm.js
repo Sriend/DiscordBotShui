cead2da2259155c5ec6b69ac6394f84ff99622d2
Sriend <happy.sriend@gmail.com>
2020-07-18 13:07:35 +0700

    Fix filter farm

--- Commands/Toram/Farm.js
+++ Commands/Toram/Farm.js
@@ -7,23 +7,27 @@ module.exports = {
     usage: "name [<name>|<add>|<update>|<delete>]",
     run: async (client, message, args)=>{
         let farm = new Array("metal","cloth","beast","wood","medicine","mana");
-        let name = farm.filter(f=>{ return f === args[0];})[0];
+        let name = farm.filter(f=>{ return f.includes(args[0]);})[0];
         console.log(name);
         if(!name) return message.channel.send("input farm list!");
         let embed = new Discord.MessageEmbed();
-        //let data  = require("");
         embed.setTitle(`${name} Materials`);
         switch(name){
             case "metal":
                 embed.addField("Item Name","Cracked Halo",true);
                 embed.addField("Monster Name","Bubble Angel",true);
-                embed.addField("Map Location","Shrine of Gods",true);
-            break;
+                embed.addField("Map Location","Shrine of the Gods Area 2",true);
+                break;
             case "cloth":
+                break;
             case "beast":
+                break;
             case "wood":
+                break;
             case "medicine":
+                break;
             case "mana":
+                break;
             default:
                 embed.setDescription("Comming Soon");
                 break;
