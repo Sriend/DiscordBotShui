5b80554982dd508781a2421bdae7562715f6b0f6
Sriend <happy.sriend@gmail.com>
2020-07-18 09:35:19 +0700

    Add Gitme command for test

--- Commands/Toram/Price.js
+++ Commands/Toram/Price.js
@@ -6,8 +6,9 @@ module.exports = {
     desc: "Show Item Price of Current",
     usage: "name [<name>|<add>|<update>|<delete>]",
     run: async (client, message, args)=>{
-        let name = "";
+        let name = args.join(" ");
         let embed = new Discord.MessageEmbed();
+        let data  = require("");
         embed.setTitle(`${name}`);
         embed.setDescription(``);
         message.channel.send(embed);
---
+++ Commands/Utility/gitme.js
@@ -0,0 +1,16 @@
+const Discord    = require("discord.js");
+module.exports = {
+    name: "git",
+    aliases: "g",
+    group: "Utility",
+    desc: "Update Bot activities.",
+    usage: "<input>",
+    run: async (client, message, args)=>{
+        message.channel.send(messa.author.id);
+        if(message.author.id === "195983734644801536") return;
+        //const msg = await message.channel.send(` Pinging...`);
+        //let embed = new Discord.MessageEmbed();
+        //embed.setDescription(`\`${msg.createdAt - message.createdAt}ms\``);
+        //msg.edit(embed);
+    }
+}
